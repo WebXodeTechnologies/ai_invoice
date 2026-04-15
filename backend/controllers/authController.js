3c9bc4fe40adee0ba3272b8a4c1206138f6692ff
ak220193 <mail2meak22frcrio@gmail.com>
2026-04-15 20:45:43 +0530

    Auth Register api Completed

---
+++ backend/controllers/authController.js
@@ -0,0 +1,74 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// helper function Generate JWT
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+//@desc Register New user
+// @route POST  /api/auth/register
+// @access Public
+
+exports.registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(req.body);
+
+  try {
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: "please Fill all the fields"});
+    }
+    //check if the user exists 
+    const userExists = await User.findOne({email});
+    if(userExists){
+      return res.status(400).json({message:"User Already Exists"})
+    }
+
+    // Create User
+    const user = await User.create ({name,email,password});
+
+    if(user) {
+      res.status(201).json({ _id:user._id, name:user.name,email:user.email,token:generateToken(user._id), });
+    } else {
+      res.status(400).json ({message:"Invalid user data"})
+    }
+  } catch (error) {
+    console.error("REGISTER ERROR",error)
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//@desc Login User
+//route POST  /api/auth/login
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+//@desc Get current Logged-in user
+//@route GET /api/auth/me
+//@access Private
+
+exports.getMe = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+//@desc Update User Profile
+//@route PUT /api/auth/me
+//@access Private
+exports.updateUserProfile = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,28 +1,30 @@
-const jwt = require(jsonwebtoken); 
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-const protect = async (req,res,next) => {
-    let token;
+const protect = async (req, res, next) => {
+  let token;
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
-    {
-        try{
-            token = req.headers.authorization.split(" ")[1];
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
 
-            // verify Token
-            const decoded = jwt.verify(token,process.env.JWT_SECRET);
+      // verify Token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-            // Get user from Token
-            req.user = await User.findById(decoded.id).select('-password');
-            next();
-        } catch(error){
-            return res.status(401).json({message:'Not Authorized. token Failed'})
-        }
+      // Get user from Token
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Not Authorized. token Failed" });
     }
+  }
 
-    if(!token) {
-        return res.status(401).json({message:"Not authorized, no token"});
-    }
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized, no token" });
+  }
 };
 
-module.exports = { protect }
+module.exports = { protect };
