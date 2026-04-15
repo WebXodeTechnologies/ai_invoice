d9919c61e595c9fe5251854c9a2d528531007085
ak220193 <mail2meak22frcrio@gmail.com>
2026-04-15 12:47:16 +0530

    Monogo db conneected

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require ("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db"); 
+
+
+const app = express();
+
+// Middleware to handle CORS 
+
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders:["Content-type", "Authorization"],
+    })
+)
+
+// Connect Database
+connectDB();
+
+// Middleware 
+
+app.use(express.json()); 
+
+// Routes here 
+
+// start server
+const PORT = process.env.PORT || 5000;
+app.listen (PORT, ()=> console.log(`Server Running on port ${PORT}`))
