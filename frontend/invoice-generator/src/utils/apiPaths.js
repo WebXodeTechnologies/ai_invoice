ff7df1fb125efd65eedb75183c9e601727740571
ak220193 <mail2meak22frcrio@gmail.com>
2026-04-29 17:19:27 +0530

    Api paths and axios instance connected

---
+++ frontend/invoice-generator/src/utils/apiPaths.js
@@ -0,0 +1,23 @@
+export const BASE_URL = "http://localhost:8000";
+
+export const API_PATHS = {
+    AUTH: {
+        REGISTER : "/api/auth/register" , //signup
+        LOGIN : "/api/auth/login", //authenticate user login and return jwt login
+        GET_PROFILE: "/api/auth/me", // get logged-in user details 
+        UPDATE_PROFILE:"/api/auth/me", // update profile details (PUT)
+    },
+    INVOICE: {
+        CREATE:"/api/invoices/",
+        GET_ALL_INVOICES: "/api/invoices/",
+        GET_INVOICE_BY_ID: (id)=> `api/invoices/${id}`,
+        UPDATE_INVOICE: (id)=> `api/invoices/${id}`,
+        DELETE_INVOICE:(id)=> `api/invoices/${id}`,
+    },
+
+    AI:{
+        PARSE_INVOICE_TEXT: '/api/ai/parse-text',
+        GENERATE_REMAINDER: '/api/ai/generate-reminder',
+        GET_DASHBOARD_SUMMARY: '/api/ai/dashboard-summary',
+    }
+};
